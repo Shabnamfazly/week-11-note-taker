b34d7805d98307ec441fe74c708f3bee686f386b
Fazly <shabnamfazly@gmail.com>
2023-07-19 20:00:09 +0100

    added post and get

--- server.js
+++ server.js
@@ -3,19 +3,25 @@ const path= require('path');
 const fs = require('fs')
 const PORT= process.env.PORT||3001
 const app= express()
+const storage = require("db/storage.js")
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
 app.use(express.static("public"))
-app.get('/',(rec,res)=>{
+app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"/public/index.html"))
 })
 
-app.get('/notes',(rec,res)=>{
+app.post('/notes',(req,res)=>{
+    storage.addNote(req.body).then((note)=>res.json(note)).catch((err)=>res.status(500).json(err))
+
+})
+
+app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname,"/public/notes.html"))
 })
-app.get("/api/notes",(rec,res)=>{
+app.get("/notes",(req,res)=>{
     fs.readFile("./db/db.json","utf8",(err,data)=>{
         if (err){
             console.error(err)
