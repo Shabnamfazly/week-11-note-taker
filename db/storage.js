d8a9d8a0cb7d0309d48d79353df0436535466200
Fazly <shabnamfazly@gmail.com>
2023-07-22 16:13:59 +0100

    fixed error

--- db/storage.js
+++ db/storage.js
@@ -1,26 +1,29 @@
-const util = require('util')
-const fs = require('fs')
+const util = require("util");
+const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-
-const readFileAsync = util.promisify(fs.readFile)
-const writeFileAsync = util.promisify(fs.writeFile)
-class Storage{
-    read(){
-        return readFileAsync("db/db.json","utf-8")
-    }
-    write(note){
-        return writeFileAsync("db/db.json",JSON.stringify(note))
-    }
-    getNote(){
-        return this.read().then((notes)=>{
-            let parsedNotes;
-            return parsedNotes
-        })
-    }
-    addNote(note){
-        const {title,text}= note;
-        const newNote = {title,text,id:uuidv4()}
-        return this.getNote().then((notes)=>[...notes,newNote]).then((updatedNotes)=>this.write(updatedNotes)).then(()=>newNote)
-    }
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+class Storage {
+  read() {
+    return readFileAsync("db/db.json", "utf-8");
+  }
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
+  getNote() {
+    return this.read().then((notes) => {
+      let parsedNotes = JSON.parse(notes);
+      return parsedNotes;
+    });
+  }
+  addNote(note) {
+    const { title, text } = note;
+    const newNote = { title, text, id: uuidv4() };
+    console.log(newNote);
+    return this.getNote()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
 }
 module.exports = new Storage();
--- server.js
+++ server.js
@@ -1,43 +1,40 @@
-const express = require('express');
-const path= require('path');
-const fs = require('fs')
-const PORT= process.env.PORT||3001
-const app= express()
-const storage = require("./db/storage")
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-
-app.use(express.static("public"))
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"/public/index.html"))
-})
-
-app.post('/api/notes',(req,res)=>{
-    storage.addNote(req.body).then((note)=>res.json(note))
-
-})
-
-app.get('/notes',(req,res)=>{
-    res.sendFile(path.join(__dirname,"/public/notes.html"))
-    
-})
-app.get("/notes",(req,res)=>{
-    fs.readFile("./db/db.json","utf8",(err,data)=>{
-        if (err){
-            console.error(err)
-            res.status(500).send('This is broken')
-return
-        }
-        try{
-            const notes=JSON.parse(data)
-            res.json(notes)
-        }catch(parseError){
-            console.error(parseError)
-            res.status(500).send('error parsing data')
-        }
-    })
-})
-app.listen(PORT,()=>{
-    console.log(`app listening @: ${PORT}`)
-}) 
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const storage = require("./db/storage");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+  storage.addNote(req.body);
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.delete("/api/notes/:id", (req, res) => {});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("This is broken");
+      return;
+    }
+    try {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    } catch (parseError) {
+      console.error(parseError);
+      res.status(500).send("error parsing data");
+    }
+  });
+});
+app.listen(PORT, () => {
+  console.log(`app listening @: ${PORT}`);
+});
